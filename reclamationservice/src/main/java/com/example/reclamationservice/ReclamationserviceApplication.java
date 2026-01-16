ba380f5f3b0e088714f5ffa941e96b62d71bf02f
Saleck07 <salekamein@gmail.com>
2026-01-16 22:43:47 +0000

    first commit

---
+++ reclamationservice/src/main/java/com/example/reclamationservice/ReclamationserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.reclamationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReclamationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReclamationserviceApplication.class, args);
+	}
+
+}
